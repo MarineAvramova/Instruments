121ad20ee19e1118befbb70b838930cbf4ea9bbb
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-01-24 12:47:23 +0100

    Update Instruments.java

--- src/Instruments.java
+++ src/Instruments.java
@@ -1,6 +1,6 @@
 abstract class Instruments {
-    String name;
-    int price;
+   private String name;
+    private int price;
     abstract void play();//abstract method doesnt have a body
     static { //Static code block
         System.out.println("The static block initization in abstract class Instruments");
